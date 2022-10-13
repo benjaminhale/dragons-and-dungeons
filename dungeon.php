0399e958182e20d8847b59be1a422d5d4d62fddd
benjaminhale <33043151+benjaminhale@users.noreply.github.com>
2022-10-12 21:21:20 -0600

    Add files via upload

---
+++ dungeon.php
@@ -0,0 +1,17 @@
+<?php
+$dataArr = array(
+    "text" => 'R                '.
+              '/n         I      '.
+              '/n—             '.
+              '/n——      {{{     '.
+              '/n——            '.
+              '/n—              '.
+              '/n            ,     '.
+              '/n————————  Noemie is the best  ',
+              
+    // "text" => "R /n          /n ",
+    "tag" => "welcome"
+);
+$myJSON = json_encode($dataArr);
+echo $myJSON;
+?>
