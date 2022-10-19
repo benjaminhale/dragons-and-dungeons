cc5be0551aa26e9ce1f2a0d60f3b0d418a8257d1
benjaminhale <33043151+benjaminhale@users.noreply.github.com>
2022-10-18 20:27:24 -0600

    Delete dragons_and_dungeons/dragons_and_dungeons directory

--- dragons_and_dungeons/dragons_and_dungeons/head.php
+++
@@ -1,6 +0,0 @@
-<head>
-    <link rel="stylesheet" href="styles.css">
-    <title> Dragons and Dungeons </title>
-    <!-- <script type = "text/javascript" src="Display.js"></script> -->
-    <script src="Display.js"></script>
-</head>
