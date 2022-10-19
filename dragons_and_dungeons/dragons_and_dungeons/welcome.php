3fec2e36509f0bd54235b5c7e5b26020892fb3c0
benjaminhale <33043151+benjaminhale@users.noreply.github.com>
2022-10-18 20:27:16 -0600

    Delete welcome.php

--- dragons_and_dungeons/dragons_and_dungeons/welcome.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dataArr = array(
-    "text" => "WELCOME to Dragons and Dungeons!</br>Adventure awaits....<br>Press SPACEBAR to forge ahead.",
-    "tag" => "welcome"
-);
-$myJSON = json_encode($dataArr);
-echo $myJSON;
-?>
